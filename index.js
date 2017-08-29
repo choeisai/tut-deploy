d8cda9d000acabadbc7c397cb7e6d44a127cc5ce
Chalermrat Srinara <schalermrat@gmail.com>
2017-08-29 15:52:16 +0700

    feat(user): Add user api

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// User api
+app.get('/user', (req, res) => {
+  res.send('Hi Bob!')
+})
+
 app.listen(3000, () => {
   console.log('App listening on port 3000')
 })
